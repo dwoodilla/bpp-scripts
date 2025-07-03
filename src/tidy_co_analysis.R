25b22fa9025cc2c8cab19056aec2148952bd4230
dwoodilla <david_mike_woodilla@brown.edu>
2025-07-03 13:38:16 -0400

    tidied combined_df

--- src/tidy_co_analysis.R
+++ src/tidy_co_analysis.R
@@ -1,36 +1,49 @@
 library(tidyverse)
 library(openair)
 library(checkmate) # type checking
+library(glue)
 # library(stringr) # Used for df_parser function
 
 unlink("./plots/*.png", expand=TRUE) # Hit Ctrl-Enter on this line to clear temporary files.
 
 combined_df = read.csv("./clean_data/all_co_temp_rh_2022.csv")
 combined_df$date = as.POSIXct(combined_df$date, tz="UTC")
-combined_df$date = with_tz(combined_df$date, tzone="America/New_York") # Allows OpenAir to account for EST/EDT
-valid_cols = colnames(combined_df)
+tidy_combined_df = combined_df %>% pivot_longer( # Convert combined_df to tidy format
+    cols = -date,
+    names_to = c("parameter","sensor","location"),
+    values_to = "value",
+    names_pattern = "([^_]+)_([^_]+)_(.+)"
+)
+# rownames(combined_df) = combined_df$date
+tidy_combined_df$date = with_tz(tidy_combined_df$date, tzone="America/New_York") # Allows OpenAir to account for EST/EDT
+valid_cols = colnames(tidy_combined_df)
 
-assert_df = function(df, ...) {
+assert_df = function(df) {
     # Assert df is a non-empty DataFrame with double and POSIXct columns with names from combined_df
     # NOTE: These checks do not perfectly filter out invalid dataframes, but do assert expectations that the rest of the script relies on.
-    assert_data_frame(df, types=c("double", "POSIXct"), all.missing=FALSE, min.cols=2)
-    assert_subset(colnames(df), valid_cols, empty.ok=FALSE)
+    assert_data_frame(df, ncols=5)
+    assert_set_equal(colnames(df), valid_cols)
     assert_posixct(df$date, any.missing=FALSE)
-    for (col in ...) {
-        assert_subset(col, colnames(df))
-    }
 }
 
-tidy_co_stats = function(df, ...) {
-    args = list(...)
-    assert_df(df, args$x, args$y)
+tidy_co_stats = function(df, title) {
+    assert_df(df)
+    df = df %>% filter(parameter=="co")
     ggplot(
         data=df,
         mapping=aes(
-            x=args$x,
-            y=args,
-            color=if("color" in names(args)) args$color else NULL
+            x=interaction(sensor, location, sep=" @ "),
+            y=value,
+            fill=sensor
         )
     ) +
-    geom_boxplot(title=)
+    geom_boxplot() + 
+    labs(
+        title=title,
+        x = "Sensor type @ Location",
+        y = "CO (ppm)"
+    )
+    ggsave(glue("./plots/co_stats_{title}.png"))
 }
+
+tidy_co_stats(tidy_combined_df, title="Test plot")
