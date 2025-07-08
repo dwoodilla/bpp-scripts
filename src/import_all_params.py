016744ec1e8a536a4a214a0fbed75985194939dc
dwoodilla <david_mike_woodilla@brown.edu>
2025-07-07 21:59:41 -0400

    bits of import

--- src/import_all_params.py
+++ src/import_all_params.py
@@ -5,22 +5,21 @@ import pyaqsapi as aqs
 from datetime import date
 from functools import reduce
 
-bcn_files = glob("./data/beaco2n/*.csv")
-qaq_files = glob("./data/quantaq/*.csv")
-aqs_files = glob("./data/aqs/*.csv")
+bcn_files = glob("../data/beaco2n/*.csv")
+qaq_files = glob("../data/quantaq/*.csv")
+aqs_files = glob("../data/aqs/*.csv")
 
 bcn_df = {os.path.splitext(os.path.basename(f))[0] : pd.read_csv(f) for f in bcn_files}
 qaq_df = {os.path.splitext(os.path.basename(f))[0] : pd.read_csv(f) for f in qaq_files}
 aqs_df = {os.path.splitext(os.path.basename(f))[0] : pd.read_csv(f) for f in aqs_files}
 
 def clean_bcn(df : pd.DataFrame)-> pd.DataFrame:
-    df.rename(columns={"datetime":"date"}, inplace=True)
+    df.rename(columns={"datetime":"date", "pm2_5":"pm25"}, inplace=True)
     df["date"] = pd.to_datetime(df["date"], utc=True).dt.round("h")
     df.drop(columns=["local_timestamp","epoch","node_file_id", "node_id"], inplace=True)
     df.rename({"co_corrected":"co"})
     wrk_aux_cols = df.filter(regex=r"_wrk_aux$").columns
     df.rename(columns={col : col.replace("_wrk_aux", "_raw") for col in wrk_aux_cols}, inplace=True)
-    df.dropna(how="all", subset=df.columns, inplace=True)
 
     df = df.melt(
         id_vars=["date"],
@@ -29,12 +28,24 @@ def clean_bcn(df : pd.DataFrame)-> pd.DataFrame:
     )
     df = df.assign(corrected=lambda df_arg : ~df_arg["parameter"].str.endswith("_raw")) # '~' is bitwise NOT
     df["parameter"] = df["parameter"].str.removesuffix("_raw").str.removesuffix("_corrected")
+    df.dropna(how="all", subset="value", inplace=True)
     return df
 
 def clean_qaq(df :pd.DataFrame)-> pd.DataFrame :
     df.rename(columns={"period_start_utc":"date"}, inplace=True)
     df["date"] = pd.to_datetime(df["date"], utc=True)
-    df.drop(columns=["period_start","period_end","period_end_utc","sn","n_datepoints"])
+    df.drop(columns=["period_start","period_end","period_end_utc","sn","n_datapoints"], inplace=True)
+
+    df = df.melt(
+        id_vars=["date"],
+        var_name="parameter",
+        value_name="value"
+    )
+    df.dropna(how="all", subset="value", inplace=True)
+    df = df.assign(corrected=lambda df_arg : True)
+    return df
     
 
 bcn_df = {site : clean_bcn(df) for site, df in bcn_df.items()}
+qaq_df = {site : clean_qaq(df) for site, df in qaq_df.items()}
+print(qaq_df["dpw"].head())
